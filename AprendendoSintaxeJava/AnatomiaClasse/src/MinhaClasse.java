15c7ba9a8ccb0eacc34cfc4f3cd7bc660896685b
Carlos Pavão - Windows <carlos.henrique93@msn.com>
2024-04-26 04:10:34 -0300

    Revert "Delete AprendendoSintaxeJava/AnatomiaClasse directory"
    This reverts commit a508397543dac701f8ffa6d901002e9c656bd122.

---
+++ AprendendoSintaxeJava/AnatomiaClasse/src/MinhaClasse.java
@@ -0,0 +1,27 @@
+public class MinhaClasse {
+    public static void main(String[] args) {
+        System.out.println("Olá Turma, Sejam todos bem-vindos");
+
+        //Variáveis MAIUSCULAS entedemos como final não pode ser alteradas
+        final String BR = "Brasil";
+        // só é possivel nomear variaveis com letras e com _ e $ de outras formas não será permitido
+
+        //exemplos de variavel bem definida e clara
+
+        String meuNome = "Carlos";
+        int anoFabricacao = 2024;
+
+        boolean verdadeira = true;
+
+        String nomeCompleto = nomeCompleto("Carlos", "Pavão");
+
+        System.out.println(nomeCompleto);
+
+
+
+    }
+
+    public  static String nomeCompleto(String primeiroNome, String segundoNome){
+        return "Resultado do método: " +primeiroNome.concat(" ").concat(segundoNome);
+    }
+}
